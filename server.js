aae472bc2af24ccf1e0ccae384dcae1a4d4403d2
Kevin nX_ J <contact@n-x.xyz>
2021-11-23 13:57:44 +0100

    Finished Forum Tracking
    - Finalized Forum Tracking
    - Added all current user posts from employees as default files to the repository

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Globals
 require('./modules/globals.js');
 
-global.debug = true;
+global.debug = false;
 
 client.on("ready", async () => {
     console.log(`✔ Logged in as ${client.user.username}`);
 
-    let server = await client.guilds.cache.find(r => r.id === "886942126053679104");
+    let server = await client.guilds.cache.find(r => r.id === process.env.server_id);
 
     if(server === undefined){
         console.log(`ERROR: Could not find Guild by specified ID.`);
@@ -17,22 +17,35 @@ client.on("ready", async () => {
     }
 
     // Start Twitter listener
-    let twitterChannel = await server.channels.cache.get("912336981638402068");
+    let twitterChannel = await server.channels.cache.get(process.env.channel_twitter);
     if(twitterChannel !== undefined)
         twitter.startTwitter(client, twitterChannel);
 
     // Start Twitch listener
-    let twitchChannel = await server.channels.cache.get("912336981638402068");
+    let twitchChannel = await server.channels.cache.get(process.env.channel_twitch);
     if(twitchChannel !== undefined)
         twitch.startTwitch(client, twitchChannel);
 
     // Start Discourse Tracking (Forum Tracker)
     await require('./modules/forumTracker/getCategories');
 
-    // Loop employees ?
-
-    let list = employees.members;
-    await new ForumTracker(list[1].username);
+    // Loop all employees for the Forum Tracker
+    try {
+        let list = employees.members; // these are all known crate entertainment employees
+        for (let user in list) {
+            if (list[user].id !== undefined && list[user].username !== undefined) {
+                let interv = (Math.floor(Math.random() * 100) + 60) * 5000; // random interval calculation so we don't rate limit ourselves
+                setInterval(
+                    async function () {
+                        await new ForumTracker(list[user].username);
+                    }
+                    , interv
+                );
+                await new ForumTracker(list[user].username);
+                console.log(`Tracker created for ${list[user].username} with a interval of ${(interv / 1000)}s`.blue);
+            }
+        }
+    }catch(error) { console.error(error);}
 
 
 });
